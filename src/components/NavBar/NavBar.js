1f52984b66162cdff9754c1e08f535f0cda1a900
Maick Duarte <maickduarte.2@hotmail.com>
2025-06-04 23:49:55 -0300

    Criação do NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,7 +1,16 @@
+import Link from "next/link";
+
 export default function NavBar() {
   return (
-    <nav>      
-        NavBar da pagina
+    <nav className="w-full h-16 flex justify-between items-center px-16 border-b-1 border-gray-200">
+        <Link href="/" className="text-2xl font-bold">
+        <span>&lt;Maick Duarte /&gt;</span>
+        </Link>
+        <div className="flex flex-row items-center justify-center gap-x-8">
+            <Link href="/">Inicio</Link>
+            <Link href="/sobre">Sobre</Link>
+            <Link href="/contato">Contato</Link>
+        </div>
     </nav>
   )
 }
