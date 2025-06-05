94bfe05c700bcb147a5c2b811bfba27e8721ef6f
Maick Duarte <maickduarte.2@hotmail.com>
2025-06-04 22:52:04 -0300

    padronizando componentes e estrutura de projeto

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,15 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/Footer/Footer";
+import NavBar from "@/components/NavBar/NavBar";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>
+      <body>
+        <NavBar />
         {children}
+        <Footer />  
       </body>
     </html>
   );
