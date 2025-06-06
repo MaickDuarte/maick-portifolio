a7bd78b5fe28be408b09817de09d1772bd770528
Maick Duarte <maickduarte.2@hotmail.com>
2025-06-05 22:33:21 -0300

    ajustes variaveis globais e hover do link

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <nav className="w-full h-16 flex justify-between items-center px-16 border-b-1 border-gray-200">
+    <nav className="bg-darker w-full h-16 flex justify-between items-center px-16 border-b-1 border-gray-200 sticky top-0">
         <Link href="/" className="text-2xl font-bold">
         <span>&lt;Maick Duarte /&gt;</span>
         </Link>
         <div className="flex flex-row items-center justify-center gap-x-8">
-            <Link href="/">Inicio</Link>
-            <Link href="/sobre">Sobre</Link>
-            <Link href="/contato">Contato</Link>
+            <Link href="/" className="hover:transform hover:scale-110">Home</Link>
+            <Link href="/sobre" className="hover:transform hover:scale-110">Sobre</Link>
+            <Link href="/contato" className="hover:transform hover:scale-110">Contato</Link>
         </div>
     </nav>
   )
